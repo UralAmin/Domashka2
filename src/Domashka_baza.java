01ce6e4749e2a04c69a2c66f3c63f13ee4fe20d4
Aminefff <aminefff@Yandex.ru>
2022-07-21 16:34:36 +0500

    1.1

---
+++ src/Domashka_baza.java
@@ -0,0 +1,12 @@
+public class Domashka_baza {
+    public static void main(String[] args) {
+
+
+        for (int i = 1; i <= 100; i++) {
+            System.out.print(i+"a ");
+
+        }
+
+    }
+}
+
