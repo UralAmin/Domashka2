3b51e741a3ca52ea584c284cf8f622e277d11969
Aminefff <aminefff@Yandex.ru>
2022-07-21 17:08:31 +0500

    1.2

--- src/Domashka_baza.java
+++ src/Domashka_baza.java
@@ -7,5 +7,21 @@ public class Domashka_baza {
 
         }
 
+        System.out.println();
+
+        int ageChildren = 3;
+        if (ageChildren >=1  && ageChildren <=6) {
+            System.out.println("пойдет в детский сад");}
+            else if (ageChildren <=11) {
+            System.out.println("пойдет в младшую школу");}
+            else if (ageChildren <=17) {
+            System.out.println("пойдет в среднюю школу");}
+            else  { System.out.println("пойдет в университет");
+        }
+
+
+            
+
+
     }
 }
