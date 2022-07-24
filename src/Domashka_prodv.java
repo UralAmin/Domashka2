043cf37cdcf164a268166599764172359f18c385
Aminefff <aminefff@Yandex.ru>
2022-07-25 00:36:12 +0500

    2.3

--- src/Domashka_prodv.java
+++ src/Domashka_prodv.java
@@ -28,10 +28,38 @@ public class Domashka_prodv {
                 System.out.print(array[l]);}
         }
 
+        System.out.println();
 
 
 
+        //Часть 3 задачи
 
+        boolean hasFuel = false;
+        boolean hasElectricsProblem = false;
+        boolean hasMotorProblem = false;
+        boolean hasTransmissionProblem = true;
+        boolean hasWheelsProblem = false;
+
+
+        boolean a = hasFuel;
+        boolean b = hasElectricsProblem;
+        boolean c = hasMotorProblem;
+        boolean d = hasTransmissionProblem;
+        boolean e = hasWheelsProblem;
+        double sum = 0;
+
+
+        if ((a) && (!b&&!c&&!d&&!e)) {
+            System.out.println("Такого быть не может");
+        } else if ((!a) && (!b&&!c&&!d&&!e)) { sum = 1000; System.out.println(sum);
+        } else if (!a && !b && c && !d && !e) { sum = 10000;  System.out.println(sum);
+        } else if (!a && b && !c && !d && !e) { sum = 5000;  System.out.println(sum);
+        } else if (!a && !b && !c && d && !e) { sum = 4000;  System.out.println(sum);
+        } else if (!a && !b && !c && d && e) { sum = 2000;  System.out.println(sum);
+        }
+
+
+        
 
 
 
